adac8f8688d681367d96f68a519c82b199cccaf2
Suhaib Khan <suhaib0900@gmail.com>
2024-10-19 09:00:47 +0100

    Fresh Setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+const { PORT } = require('./config/serverConfig');
+
 const fastify = require('fastify')({ logger: true });
-const PORT=3000;
-async function start() {
-    try{
-        await fastify.listen({port:PORT});
-        console.log(`Server is running on port ${PORT}`);
-    }
-    catch(err){
-        fastify.log.error(err);
-        process.exit(1);
-    }
+async function startServer() {
+  try {
+    await fastify.listen({ port: PORT });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
-start();
+startServer();
